1a14fe0322b794f94e74957d95b1315be0a2e072
DRomanM85 <d.roman.m@icloud.com>
2024-01-26 00:59:19 +0200

    DRAFT TASK2

--- HomeWork1/js/functionUser.js
+++ HomeWork1/js/functionUser.js
@@ -1,31 +1,58 @@
- function getUserName(messageForUser)
+//Retrieve the user's name
+function getUserName(messageForUser)
 {
     let userName = prompt(messageForUser, 'YOUR NAME');
-    
-    if(checkAnswerUser(userName)){
-        return userName;
-    }
-    else
-    {
-        if(userName === "YOUR NAME"){
-            getUserName('Напевно моя підказка спантеличила🤔 тебе. \nЗамість "YOUR NAME" впиши🖊️ своє і`мя.');
-        }
-        else if(userName === ""){
-            getUserName('Упс😬!!! Якто сталося, ти забув🤷‍♂️ написати📝 своє імя.');
+    try {
+        if(checkAnswerUser(userName)){
+            return userName;
         }
         else
         {
-            return null;
+            if(userName === "YOUR NAME"){
+                getUserName('Напевно моя підказка спантеличила🤔 тебе. \nЗамість "YOUR NAME" впиши🖊️ своє і`мя.');
+            }
+            else if(userName === ""){
+                getUserName('Упс😬!!! Якто сталося, ти забув🤷‍♂️ написати📝 своє імя.');
+            }
+            else
+            {
+                return null;
+            }
         }
+    } catch (error) {
+        console.error(error);
     }
+    
 };
 
+//Validate the input
 function checkAnswerUser(userName)
 {
     return userName != null && userName != "" && userName != "YOUR NAME";
 };
 
+//Send a message
 function sendMessageForUser(messageUser)
 {
     alert(messageUser);
 };
+
+function getUserValue(messageForUser)
+{
+    var userValue = prompt(messageForUser);
+    if(checkAnswerUser(userValue)){
+        return userValue;
+    }
+    else
+    {
+        getUserValue('Яка прикристь, ти відправив порожній рядок.');
+    }
+    
+}
+
+function randomConcatenation(userArray) {
+    const shuffledArray = userArray.sort(() => Math.random() - 0.5);
+
+    const result = shuffledArray.join(' - ');
+    return result;
+}
