074e8f6f5b8f90e712bcfc924a3db01ea08eb80f
DRomanM85 <d.roman.m@icloud.com>
2024-03-10 21:47:22 +0200

    lesson 10

---
+++ HomeWork/HomeWorks/LESSON10/js/main.js
@@ -0,0 +1,17 @@
+
+  let tbPassword = document.querySelector('#textBoxPassword');
+  let lbhint = document.querySelector('#psw');
+
+  tbPassword.addEventListener('mouseover', function () {
+    lbhint.classList.toggle('pswhint');
+  });
+
+  tbPassword.addEventListener('mouseout', function () {
+    lbhint.classList.toggle('pswhint');
+  });
+
+
+  
+    
+  
+  
